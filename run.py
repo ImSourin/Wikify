4795c0f041b47b1905ad67c84f306b7167e741db
unknown <sourin.chakrabarti@gmail.com>
2019-05-15 15:07:28 +0530

    Added code for testing the server in test.py

--- run.py
+++ run.py
@@ -1,6 +1,8 @@
+'''Import the app and database object.'''
 from source.server import app
 from source.server import db
 
 if __name__=="__main__":
+    '''Create database(if not already present) and run the app.'''
     db.create_all()
     app.run(debug=True)
---
+++ test/test.py
@@ -0,0 +1,25 @@
+import requests
+import json
+
+localurl = "http://127.0.0.1:5000/api/v1"
+hostedurl = "http://sourin.pythonanywhere.com/api/v1"
+
+url = hostedurl
+'''uncomment the below line for using the local api service. This can only be done after api is running locally.'''
+# url = localurl
+
+'''Create'''
+json_obj = {"name": "Virat_Kohli", "params": ["type", "api_urls.references.reference_lists[0].id", "thumbnail.source"]}
+print(requests.post(url=url,headers={"content-type":"application/json"},data=json.dumps(json_obj)).content)
+
+'''Read'''
+json_obj = {"name": "Virat_Kohli", "params": ["api_urls.references.reference_lists[0].id", "thumbnail.source"]}
+print(requests.get(url=url,headers={"content-type":"application/json"},data=json.dumps(json_obj)).content)
+
+'''Update'''
+json_obj = {"name": "Virat_Kohli", "add": ["title"], "remove": ["type"]}
+print(requests.put(url=url,headers={"content-type":"application/json"},data=json.dumps(json_obj)).content)
+
+'''Delete'''
+json_obj = {"name": "Virat_Kohli"}
+print(requests.delete(url=url,headers={"content-type":"application/json"},data=json.dumps(json_obj)).content)
